2a4123618f741c132d46eaadd8d06040d4891f51
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-05-17 17:59:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,27 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	const result = [];
+    let currentChunk = [];
+    let currentSum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (currentSum + arr[i] <= n) {
+            currentChunk.push(arr[i]);
+            currentSum += arr[i];
+        } else {
+            result.push(currentChunk);
+            currentChunk = [arr[i]];
+            currentSum = arr[i];
+        }
+    }
+
+    if (currentChunk.length > 0) {
+        result.push(currentChunk);
+    }
+
+    return result;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+   const n = prompt("Enter n: ");
+   alert(JSON.stringify(divide(arr, n)));
